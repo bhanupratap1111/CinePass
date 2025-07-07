8c358f2f2408e2a563f566530073bde60f74194d
bhanupratap1111 <bhanupratap221514@gmail.com>
2025-07-07 19:46:20 +0530

    done with the frontend

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection.jsx'
+import FeaturedSection from '../components/FeaturedSection.jsx'
+import TrailersSection from '../components/TrailersSection.jsx'
+
+function Home() {
+  return (
+    <>
+      <HeroSection/>
+      <FeaturedSection/>
+      <TrailersSection/>
+    </>
+  )
+}
+
+export default Home
